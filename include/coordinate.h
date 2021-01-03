348a4736386a96caf0203fe2b3531854f1b58b45
Gautam Gupta <gautam.gupta.offc@gmail.com>
2021-01-03 11:12:23 +0000

    Update coordinate.h

--- include/coordinate.h
+++ include/coordinate.h
@@ -2,6 +2,6 @@
 
 typedef struct Coordinates 
 {
-    int x, y;  //x is x-coordinate, y is y-coordinate
+    double x, y;  //x is x-coordinate, y is y-coordinate
     char audio[50]; //name of audio file
 } coords;            
