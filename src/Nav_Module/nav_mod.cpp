c0d193f4580e523d716ba6f15a41e606ebbbad18
gautam2028 <gautam.gupta.offc@gmail.com>
2021-01-03 11:06:13 +0000

    updated Nav module

--- src/Nav_Module/nav_mod.cpp
+++ src/Nav_Module/nav_mod.cpp
@@ -1,1 +1,11 @@
-//Source for the nav_mod will go here, this is me just testing github out lol
+#include <coordinate.h>
+#include <math.h>
+//#include <DUNCANS MODULE GOES HERE>
+
+int nav(coords start, coords end, double velocity){
+    double heading = atan((end.y - start.y)/(end.x - start.x));
+    double distance = sqrt(pow((end.x - start.x), 2.0) + pow((end.y - start.y), 2.0));
+    double time_needed = distance/velocity;
+    
+    //Function to drive from start to end
+}
