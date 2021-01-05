7cf605e372e4bc3ec9b909b12c44d26ff59825b8
gautam2028 <gautam.gupta.offc@gmail.com>
2021-01-05 10:20:24 +0000

    updated nav module

--- src/Nav_Module/nav_mod.cpp
+++ src/Nav_Module/nav_mod.cpp
@@ -2,16 +2,6 @@
 #include <math.h>
 #include "coordinate.h"
 
-typedef struct Coordinates 
-{
-    double x, y;  //x is x-coordinate, y is y-coordinate
-    char audio[50]; //name of audio file
-} coords;            
-
-typedef struct Time_Distance_Heading{
-    double time, distance, heading;
-} tdh;
-
 tdh nav(coords start, coords end, double velocity){
     tdh result;
     result.heading = atan((end.y - start.y)/(end.x - start.x));
