bfd27e191a1645d4de386bf0f4bd097e5259e601
gautam2028 <gautam.gupta.offc@gmail.com>
2021-01-06 09:14:56 +0000

    updated Nav module with a simple test

--- src/Nav_Module/nav_mod.cpp
+++ src/Nav_Module/nav_mod.cpp
@@ -1,6 +1,7 @@
 #include <string.h>
 #include <math.h>
 #include <coordinate.h>
+#include <stdio.h>
 
 time_dist_head nav(coords start, coords end, double velocity){
     time_dist_head result;
@@ -9,4 +10,16 @@ time_dist_head nav(coords start, coords end, double velocity){
     result.time = (0.8 * result.distance/velocity) + (2 * 0.1 * result.distance/velocity) * 2; // The second term is to account for accelerating and de-accelerating at the start and end
 
     return result;
+}
+
+int main() {
+	coords a, b;
+	time_dist_head tdh;
+
+	a.x = 0;
+	a.y = 0;
+	b.x = 1;
+	b.y = 1;
+	tdh = nav(a, b, 1);
+	printf(tdh.time, tdh.distance);
 }
