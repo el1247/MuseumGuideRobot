a9e976e5c645cdd346f566fdc673467b0cafb5df
Duncan Lowther <2402789l@student.gla.ac.uk>
2021-01-08 21:21:51 +0000

    Assign correct GPIO pins for motor controllers in gpio_assign.h

--- include/gpio_assign.h
+++ include/gpio_assign.h
@@ -2,15 +2,14 @@
 #define GPIO_ASSIGN_H
 
 /* Enum instead of #defines causes conflicting assignments to error at compile-time */
-/* XXX Unassigned pins left with implicit value -- TODO assign actual pins with explicit enum constant value */
 enum {
     /* Motor controllers (L and R) */
-    PIN_M_L_FWD,
-    PIN_M_L_REV,
-    PIN_M_L_PWM,
-    PIN_M_R_FWD,
-    PIN_M_R_REV,
-    PIN_M_R_PWM,
+    PIN_M_L_FWD = 22,
+    PIN_M_L_REV = 23,
+    PIN_M_L_PWM = 12,
+    PIN_M_R_FWD = 24,
+    PIN_M_R_REV = 25,
+    PIN_M_R_PWM = 13,
 };
 
 #endif
