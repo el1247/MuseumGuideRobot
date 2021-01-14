ff2abc1fa044f26de3488b6aff51c8f594be86a8
el1247 <ellaw99@hotmail.co.uk>
2021-01-14 09:34:18 +0000

    Logic branch initial developement
    Started writing code for the logic module.

--- include/logic.h
+++ include/logic.h
@@ -1,5 +1,22 @@
 #pragma once
+#include <string>
+#include "movement.h"
+#include "navigation.h"
+
 
 class logic {
-	bool isTour = false;
+private:
+	bool isTour = false; //Tracks if a tour is in progress
+	string pointInfo; //Container for information about a display // format may need to change
+	//instance of movement class
+	//instance of navigation class
+	//instance of QR scanning class
+	
+public:
+	void callHelp();
+	void doTour(int tourID);
+	void emergencyStop();
+	int giveInfo(int locationID);
+	void gohome();
+	void stopTour();
 };
--- src/logic.cpp
+++ src/logic.cpp
@@ -1,2 +1,53 @@
 #include "logic.h"
 
+void logic::callHelp() {
+	//stop moving, movement module
+	//send signal to help desk
+	//bring 
+	cout << "Calling helpdesk" << endl;
+}
+
+
+void logic::doTour(int tourID){
+	//lookup tour using tourID
+	//get/calculate route, nav module
+	while (isTour){
+		//move robot to next tour point
+		//say tour point
+		//get information for next tour point
+		cout << "Moving onto next tour point" << endl; //Placeholder to info programmer of exection and robots intentions
+	} 
+}
+
+
+void logic::emergencyStop(){
+	//call movement stopping method
+	//await UI confirmation 
+	cout << "Executing emergency stop" << endl; //Placeholder to info programmer of exection and robots intentions
+}
+
+
+int logic::giveInfo(int locationID){
+	//lookup location information with locationID
+	pointInfo = "This is information about a location"; //Temporary placeholder, will replace with lookup on in memory
+	if pointInfo.empty(){
+		return 0;
+	}
+	//call audio out module with this input
+	cout << "Giving tour information" << endl; //Placeholder to info programmer of exection and robots intentions
+	return 1;
+}
+
+
+void logic::goHome(){
+	//calcualte route to go home, nav module
+	//UI display of going home
+	//execute movement of robot to go home, movement module
+	cout << "Going home" << endl; //Placeholder to info programmer of exection and robots intentions
+}
+
+
+void logic::stopTour(){
+	isTour = False;
+	cout << "Stopping tour" << endl; //Placeholder to info programmer of exection and robots intentions
+}
