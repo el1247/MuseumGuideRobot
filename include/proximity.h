6056f26e6328d6d95c263eacc856a2ae2da21d60
el1247 <ellaw99@hotmail.co.uk>
2021-01-26 10:50:37 +0000

    I2Cbeginning
    Started implementation for use of pigpio

--- include/proximity.h
+++ include/proximity.h
@@ -1,8 +1,15 @@
 #pragma once
+#include <stdio.h>
+#include <iostream>
+#include <pigpio.h>
 
-class proximity {
+#define proxstart = 0b01000010 //integration time 50ms, channel interrupt, interrupt persistance setting 1, interrupt enabled, ALS power on
+
+
+class proximity{
 	private:
 		int distance; //Distance to object measured from proximity sensor
+		proximity(); //Initialiser
 	public:
-		void measuredist();
+		void measuredist(); //Method for measuring distance
 };
--- src/proximity.cpp
+++ src/proximity.cpp
@@ -3,4 +3,14 @@
 void proximity::measuredist(){
 	//Function to capture and store latest value from proximity sensor
 	//measure distance
-};
+}
+
+proximity::proximity(){
+	if (gpioInitialise() < 0){
+		std::cout << "Pigpio failed to initiaise" << std::endl; //Possibile duplicate command, may already be initialised
+	}
+	//Start pigpio
+	//Identify I2C connection
+	//Start I2C connection
+
+}
